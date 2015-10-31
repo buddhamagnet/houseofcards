beb90c3801cca39bb138444b57503b945d0cf7c3
buddhamagnet <buddhamagnet@gmail.com>
2015-10-31 16:47:47 +0000

    blew

--- partners/partners.go
+++ partners/partners.go
@@ -1,31 +1,76 @@
 package partners
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"regexp"
+)
 
-var Map = map[string]PartnerCodeValidator{
-	"singapore":     new(SingaporeAirlines),
-	"cathaypacific": new(CathayPacificAirlines),
+var (
+	patternTajHotels  *regexp.Regexp
+	presenceValidator PartnerCodeValidator
+	digitValidator    PartnerCodeValidator
+	Map               map[string]PartnerCodeValidator
+)
+
+func init() {
+	patternTajHotels = regexp.MustCompile("^10101([0-9]{9}$)")
+	presenceValidator = new(PresenceValidator)
+	presenceValidator = NewDigitValidator()
+
+	Map = map[string]PartnerCodeValidator{
+		"cathaypacific":     presenceValidator,
+		"singapore":         presenceValidator,
+		"jetairways":        presenceValidator,
+		"airasia":           presenceValidator,
+		"jetairwaysindia":   digitValidator,
+		"finnair":           digitValidator,
+		"pins":              digitValidator,
+		"tapvictoria":       digitValidator,
+		"saseurobonus":      digitValidator,
+		"iberia":            digitValidator,
+		"etihad":            digitValidator,
+		"ethiopianairlines": digitValidator,
+		"accordhotels":      NewDigitLengthValidator(10),
+		"milesandmore":      NewDigitLengthValidator(15),
+		"tajhotels":         NewPatternValidator(patternTajHotels),
+	}
 }
 
 type PartnerCodeValidator interface {
 	Validate(card string) (int, string)
 }
 
-type SingaporeAirlines struct{}
+type PresenceValidator struct{}
 
-func (s *SingaporeAirlines) Validate(card string) (int, string) {
-	return CardMandatory(card)
-}
-
-type CathayPacificAirlines struct{}
-
-func (c *CathayPacificAirlines) Validate(card string) (int, string) {
-	return CardMandatory(card)
-}
-
-func CardMandatory(card string) (int, string) {
+func (p *PresenceValidator) Validate(card string) (int, string) {
 	if card != "" {
 		return http.StatusOK, "OK"
 	}
 	return http.StatusBadRequest, "BAD"
+}
+
+func NewDigitValidator() *PatternValidator {
+	digits := regexp.MustCompile("^[0-9]+$")
+	return &PatternValidator{pattern: digits}
+}
+
+func NewDigitLengthValidator(l int) *PatternValidator {
+	digitLength := regexp.MustCompile(fmt.Sprintf("^[0-9]{%d}$", l))
+	return &PatternValidator{pattern: digitLength}
+}
+
+type PatternValidator struct {
+	pattern *regexp.Regexp
+}
+
+func (p *PatternValidator) Validate(card string) (int, string) {
+	if p.pattern.MatchString(card) {
+		return http.StatusOK, "OK"
+	}
+	return http.StatusBadRequest, "BAD"
+}
+
+func NewPatternValidator(pat *regexp.Regexp) *PatternValidator {
+	return &PatternValidator{pattern: pat}
 }
